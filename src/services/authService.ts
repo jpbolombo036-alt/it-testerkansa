82b06ad4d0a7fce4e80c3da3203dc66ae6e6c6fb
jpbolombo036-alt <jpbolombo036@gmail.com>
2026-07-13 18:12:50 +0100

    Ajoute de cors

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,9 +3,8 @@ import api from '../api/axios'
 export async function login(username: string, password: string) {
   const response = await api.post<{ accessToken: string }>('/auth/token', { username, password })
   const token = response.data.accessToken
-  
-  const userResponse = await api.get('/users/me')
-  return { token, user: userResponse.data }
+
+
 }
 
 export async function logout() {
